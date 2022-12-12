39dbec28635d1d7f5492eb1af6d2a60956a799bf
Cyrus Irani <cri23@cornell.edu>
2022-12-11 19:21:32 -0500

    added more store functionality

--- src/character.ml
+++ src/character.ml
@@ -1,3 +1,5 @@
+open Infobanks
+
 type t = {
   balance : int; (* current balance *)
   hunger : int; (* current hunger level *)
@@ -10,10 +12,10 @@ type t = {
 let make_pet pet_name = { balance = 0; hunger = 5; name = pet_name }
 
 (* prints all of the characteristics of the pet *)
-let print_pet t =
-  print_endline ("Current State of " ^ t.name);
-  print_endline ("Balance: $" ^ string_of_int t.balance);
-  print_endline ("Hunger: " ^ string_of_int t.hunger)
+let print_stats t =
+  print_endline
+    (t.name ^ "'s Stats: Balance = $" ^ string_of_int t.balance ^ "; Hunger = "
+   ^ string_of_int t.hunger ^ "/5")
 
 (* the first message the user sees when they open the game *)
 let welcome_message =
@@ -22,144 +24,20 @@ let welcome_message =
    ^ "\n"
    ^ "be taking care of your new pet! The goal of this game is to make as"
    ^ "\n"
-   ^ " much money through minigames so you can buy food and new perks for your \
+   ^ "much money through minigames so you can buy food and new perks for your \
       pet! " ^ "\n" ^ "\n"
    ^ "Let's start by naming your pet. What would you like to name your pet?");
   let x = read_line () in
+
   make_pet x
 
-let easy_trivia_bank =
-  [
-    ( 0,
-      ( "4",
-        "4: Honey",
-        "What is the only food that cannot go bad?\n\
-         1: Dark Chocolate\n\
-         2: Peanut butter\n\
-         3: Canned Tuna\n\
-         4: Honey" ) );
-    ( 1,
-      ( "2",
-        "2: Liver",
-        "What’s the heaviest organ in the human body?\n\
-         1: Brain\n\
-         2: Liver\n\
-         3: Skin\n\
-         4: Heart" ) );
-    ( 2,
-      ( "4",
-        "4: Chihuahua",
-        "Which of the following dog breeds is the smallest?\n\
-         1: Dachshund\n\
-         2: Poodle\n\
-         3: Pomeranian\n\
-         4: Chihuahua" ) );
-    ( 3,
-      ( "1",
-        "1: Blue whale",
-        "What is the biggest animal that has ever lived?\n\
-         1: Blue whale\n\
-         2: African elephant\n\
-         3: Apatosaurus\n\
-         4: Spinosaurus" ) );
-    ( 4,
-      ( "2",
-        "2: Sailfish",
-        "What is the fastest water animal?\n\
-         1: Porpoise\n\
-         2: Sailfish\n\
-         3: Flying fish\n\
-         4: Tuna" ) );
-  ]
+(* the death message *)
 
-let medium_trivia_bank =
-  [
-    ( 0,
-      ( "3",
-        "3: Italy",
-        "Which country is the largest exporter of olive oil?\n\
-         1: China\n\
-         2: Greece\n\
-         3: Italy\n\
-         4: France" ) );
-    ( 1,
-      ( "2",
-        "2: A tight rope",
-        "What does a funambulist walk on?\n\
-         1: Fire\n\
-         2: A tight rope\n\
-         3: Water\n\
-         4: Seashells" ) );
-    ( 2,
-      ( "1",
-        "1: Friendship",
-        "What is the state motto of Texas?\n\
-         1: Friendship\n\
-         2: Excelsior\n\
-         3: Liberty, Justice and Virtue\n\
-         4: Commerce" ) );
-    ( 3,
-      ( "2",
-        "2: Topeka",
-        "What is the capital city of Kansas?\n\
-         1: Little Rock\n\
-         2: Topeka\n\
-         3: Montgomery\n\
-         4: Baton Rouge" ) );
-    ( 4,
-      ( "2",
-        "2: Wyoming",
-        "Which state has the lowest cumulative population?\n\
-         1: Alaska\n\
-         2: Wyoming\n\
-         3: Vermont\n\
-         4: North Dakota" ) );
-  ]
+(* let death_message t =
+     print_endline
+       ("GAME OVER: " ^ t.name ^ "has unfortunately died from starvation :(")
 
-let hard_trivia_bank =
-  [
-    ( 0,
-      ( "3",
-        "3: 84lbs",
-        "Someone weighing 220lbs on Earth would weigh about how much on Mars?\n\
-         1: 250lb\n\
-         2: 100lbs\n\
-         3: 84lbs\n\
-         4: 200lbs" ) );
-    ( 1,
-      ( "1",
-        "1: Jupiter, Saturn, Uranus, and Neptune",
-        "What are the four planets that are known as 'Gas Giants?'\n\
-         1: Jupiter, Saturn, Uranus, and Neptune\n\
-         2: Mars, Venus, Uranus, and Saturn\n\
-         3: Mercury, Jupiter, Mars, Uranus\n\
-         4: Earth, Mars, Jupiter, Saturn" ) );
-    ( 2,
-      ( "4",
-        "4: 99.86%",
-        "The Sun's mass takes up how much of the solar system?\n\
-         1: 40.67%\n\
-         2: 90.23%\n\
-         3: 70.89%\n\
-         4: 99.86%" ) );
-    ( 3,
-      ( "2",
-        "2: Kite flying",
-        "What favorite past-time activity in the United States is considered a \
-         professional sport in Thailand?\n\
-         1: Fishing\n\
-         2: Kite flying\n\
-         3: Frisbee\n\
-         4: Bird watching" ) );
-    ( 4,
-      ( "3",
-        "3: Japan",
-        "What country has the world's largest bowling alley?\n\
-         1: United States\n\
-         2: England\n\
-         3: Japan\n\
-         4: France" ) );
-  ]
+   let check_death t = if t.hunger <= 0 then death_message else ignore *)
 
 let rec lookup k difficulty =
   match difficulty with
@@ -219,73 +97,113 @@ let rec choose_difficulty t =
   match x with
   | 1 ->
       let bonus = lookup_five_questions easy_trivia_bank in
-      { balance = t.balance + bonus; hunger = t.hunger; name = t.name }
+      { balance = t.balance + bonus; hunger = t.hunger - 1; name = t.name }
   | 2 ->
       let bonus = lookup_five_questions medium_trivia_bank * 2 in
-      { balance = t.balance + bonus; hunger = t.hunger; name = t.name }
+      { balance = t.balance + bonus; hunger = t.hunger - 1; name = t.name }
   | 3 ->
       let bonus = lookup_five_questions hard_trivia_bank * 3 in
-      { balance = t.balance + bonus; hunger = t.hunger; name = t.name }
+      { balance = t.balance + bonus; hunger = t.hunger - 1; name = t.name }
   | _ -> choose_difficulty t
 
 let trivia_minigame t =
-  print_endline ("Hey " ^ t.name ^ "! Welcome to Trivia!");
+  print_endline "\n";
+  print_endline ("Hey " ^ t.name ^ "'s Owner! Welcome to Trivia!");
   print_endline
     "You get money based on how many of the five questions you get correct! \
      Good luck!";
   choose_difficulty t
 
 let rec choose_minigame t =
+  print_endline "\n";
+  print_endline
+    "Welcome to the minigame menu!\n\
+     Here are your minigame options:\n\
+     1: Trivia\n\
+     Please choose an option!";
   let x = read_int () in
   match x with 1 -> trivia_minigame t | _ -> choose_minigame t
 
-let choose_store t =
+(* |||||||||||||||||||||||||||||STORE|||||||||||||||||||||||||||||||||||||||||*)
+let food_bank = [ (1, 1) ]
+
+let lookup_store k bank =
+  match bank with
+  | [] -> failwith "Oops!"
+  | (k', v) :: t -> if k = k' then v else lookup k t
+
+let rec food_item t =
+  print_stats t;
+  print_endline
+    "Welcome to the Grocery Store!\n\
+     Here are your food options:\n\
+     1: Biscuit $1 \n\
+     0: Main Menu \n\
+     Please choose an option!";
   let x = read_int () in
-  let y = (t, x) in
-  match y with _ -> failwith "not implemented yet"
+  match x with
+  | 1 ->
+      let y = lookup_store 1 food_bank in
+      let new_bal = t.balance - y in
+      if new_bal < 0 then
+        let _ = print_endline "CANNOT AFFORD" in
+        food_item t
+      else
+        let new_t = { balance = new_bal; hunger = t.hunger; name = t.name } in
+        new_t
+  | _ -> food_item t
+
+let rec choose_store t =
+  let x = read_int () in
+  match x with 1 -> food_item t | _ -> choose_store t
 
 let choose_home t =
   let x = read_int () in
   let y = (t, x) in
   match y with _ -> failwith "not implemented yet"
 
-let choice_of_minigames t =
+let choice_of_store_item t =
+  print_endline "\n";
+  print_stats t;
   print_endline
-    "Welcome to the minigame menu!\n\
-     Here are your minigame options:\n\
-     1: Trivia\n\
+    "Welcome to the store menu!\n\
+     Here are your store options:\n\
+     1: Food\n\
      Please choose an option!";
-  choose_minigame t
+  choose_store t
 
-(* outputs the list of activity options to the user*)
+(* |||||||||||||||||||||||||||||OPTIONS|||||||||||||||||||||||||||||||||||||||||*)
 let rec user_options t =
+  print_endline "\n";
+  print_stats t;
   print_endline
     ("Hello " ^ t.name
-   ^ "'s Owner! This is the main menu.  From here you can go to the store to \n\
+   ^ "'s Owner! This is the main menu. From here you can go to the store to\n\
      \ buy food, play minigames to win money, and go home to feed " ^ t.name
-   ^ "big stomach!\nPlease choose one of the following options:");
+   ^ "'s big stomach!\nPlease choose one of the following options:");
   print_endline "1: Store";
   print_endline "2: Minigames";
   print_endline "3: Home";
-  match read_int () with
-  | 1 ->
-      let new_t =
-        { balance = t.balance; hunger = t.hunger - 1; name = t.name }
-      in
-      choose_store new_t
-  | 2 ->
-      let new_t =
-        { balance = t.balance; hunger = t.hunger - 1; name = t.name }
-      in
-      choice_of_minigames new_t
-  | 3 ->
-      let new_t =
-        { balance = t.balance; hunger = t.hunger - 1; name = t.name }
-      in
+  match read_int_opt () with
+  | Some 1 ->
+      let new_t = { balance = t.balance; hunger = t.hunger; name = t.name } in
+      choice_of_store_item new_t
+  | Some 2 ->
+      let new_t = { balance = t.balance; hunger = t.hunger; name = t.name } in
+      let z = choose_minigame new_t in
+      user_options z
+  | Some 3 ->
+      let new_t = { balance = t.balance; hunger = t.hunger; name = t.name } in
       choose_home new_t
   | _ -> user_options t
 
+(* |||||||||||||||||||||||||||||START GAME|||||||||||||||||||||||||||||||||||||||||*)
+
+let game_loop pet =
+  let new_t = user_options pet in
+  if new_t.hunger = 0 then print_endline "YOU LOST!"
+  else print_endline "YOU WON!"
+
 let intro =
   let pet = welcome_message in
-  let new_t = user_options pet in
-  print_pet new_t
+  game_loop pet
--- src/character.mli
+++ src/character.mli
@@ -6,7 +6,7 @@ val user_options : t -> t
 val make_pet : string -> t
 (** [make_pet a] makes a new pet [a]. *)
 
-val print_pet : t -> unit
+val print_stats : t -> unit
 (** [print_pet a] outputs the status of pet [a]. *)
 
 val welcome_message : t
