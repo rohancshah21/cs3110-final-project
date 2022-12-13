17bad8fe3adc2b5fe3041ca76d2976c1a5199e9b
Rohan C. Shah <rcs353@cornell.edu>
2022-12-12 23:37:38 -0500

    character.mli changes

--- src/character.mli
+++ src/character.mli
@@ -12,4 +12,26 @@ val print_stats : t -> unit
 val welcome_message : t
 (** [make_pet a] makes a new pet [a]. *)
 
+val lookup : 'a -> ('a * 'b) list -> 'b
+(** [lookup k difficulty] finds the trivia bank questions with [difficulty] and returns it *)
+
+val lookup_one_question :
+  'a -> ('a * (string * string * string)) list -> string -> int
+(** [lookup_one_question rand difficulty question_num] randomly finds one question from the trivia bank with [difficulty] *)
+
+val five_random_numbers : 'a list -> int list -> int list
+(** [five_random_numbers difficulty acc] creates a list in [acc] that has 5 randomly generated numbers *)
+
+val lookup_five_questions : (int * (string * string * string)) list -> int
+(** [lookup_five_questions difficulty] finds 5 random questions with [difficulty] from the trivia bank and returns the amount corresponding to the credits for getting questions right based on [difficulty]*)
+
+val choose_difficulty : t -> t
+(** [choose_difficulty a] allows users to choose a question difficulty, generate the necessary questions, and ensure that they are credited appropriately *)
+
+val trivia_minigame : t -> t
+(** [trivia_minigame a] lets users initiate a game of trivia with a certain difficulty *)
+
+val choose_minigame : t -> t
+(** [choose_minigame a] lets users choose a minigame and begin the game *)
+
 val intro : unit
--- test/main.ml
+++ test/main.ml
@@ -1,16 +1,12 @@
 open OUnit2
 (* open Character *)
 
+(* Test Plan: *)
+
 let character_tests =
   [ (* ( "current game" >:: fun _ ->
          assert_equal 3 (current_game { balance = 0; currgame = 2; prevgames = 4 })
        ); *) ]
 
-let command_tests = []
-let state_tests = []
-
-let suite =
-  "test suite for final project"
-  >::: List.flatten [ character_tests; command_tests; state_tests ]
-
+let suite = "test suite for final project" >::: List.flatten [ character_tests ]
 let _ = run_test_tt_main suite
