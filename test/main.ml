13ed082ff3d848615e9d6d7ec97eaebb457edd99
Rohan C. Shah <rcs353@cornell.edu>
2022-12-13 00:08:18 -0500

    test case test

--- test/main.ml
+++ test/main.ml
@@ -1,12 +1,13 @@
 open OUnit2
-(* open Character *)
+open Character
 
-(* Test Plan: *)
+(* Test Plan: We use glass-box testing to test important and necessary functions from Character.ml *)
 
-let character_tests =
-  [ (* ( "current game" >:: fun _ ->
-         assert_equal 3 (current_game { balance = 0; currgame = 2; prevgames = 4 })
-       ); *) ]
+let intro_tests =
+  [
+    ( "looking up questions with no difficulty raises an error" >:: fun _ ->
+      assert_raises (Failure "Oops!") (lookup 1 []) );
+  ]
 
-let suite = "test suite for final project" >::: List.flatten [ character_tests ]
+let suite = "test suite for final project" >::: List.flatten [ intro_tests ]
 let _ = run_test_tt_main suite
